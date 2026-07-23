0c92a0e703d717ca412ea5ba2a1049aacc1fca8f
Xuan <xuan16022005@gmail.com>
2026-07-23 07:38:01 +0700

    fix(routing): resolve conflict when enabling both flood and traffic avoidance

--- src/New folder/frontend/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  plugins: [react()],
-  base: mode === 'production' ? '/swp391-su26-ai-audit-project-swp391_se20a04_group-01-1/' : '/',
-}))
